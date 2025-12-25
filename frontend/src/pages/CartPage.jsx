2b51df6393f9e38d6b7f6061466914c3d860f5f7
Suyog Gautam <sakurachhetri97@gmail.com>
2025-12-25 16:14:28 +0545

    finalupdate

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -47,7 +47,7 @@ const CartPage = () => {
           Add some cricket gear to get started!
         </p>
         <Link
-          to="/products"
+          to="/"
           className="inline-flex items-center px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition-colors"
         >
           <ArrowLeft className="w-5 h-5 mr-2" />
